bb728f505096baf431bac3182c452aba92c019db
DobiShredder <luire12@gmail.com>
2025-12-01 10:02:02 +0900

    update

--- 3_remd/3.1_remd/run.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-repnum=`cat temp.list | wc -l`
-
-#heat
-mpirun -np $repnum pmemd.cuda.MPI -ng $repnum -groupfile heat.group -rem 0
-
-#production
-mpirun -np $repnum pmemd.cuda.MPI -ng $repnum -groupfile remd.group -rem 1
